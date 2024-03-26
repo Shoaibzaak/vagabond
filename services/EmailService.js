ee11432ef8eb14d2824960edf93095a76df59967
shoaibzaak <shoaibzaki84@gmail.com>
2024-03-26 16:23:33 +0500

    change in the EmailServices

--- services/EmailService.js
+++ services/EmailService.js
@@ -1,91 +1,98 @@
-// const nodeMailer = require("nodemailer");
-// const fs = require("fs").promises;
+
+   
+   {/*   this code is commited due to vercel issue of not sending the html templates it's not code issue  */}
+
+// var nodeMailer = require("nodemailer");
+// const fs = require("fs");
 // const handlebars = require("handlebars");
 
-// const transporter = nodeMailer.createTransport({
+// var transporter = nodeMailer.createTransport({
 //   host: "smtp.gmail.com",
 //   port: 465,
 //   secure: true,
 //   auth: {
-//     user: process.env.MAILER_USER,
-//     pass: process.env.MAILER_PASSWORD,
+//     user: `${process.env.MAILER_USER}`,
+//     pass: `${process.env.MAILER_PASSWORD}`,
 //   },
 // });
+// let from = "no-reply <vagabond.co.usa>"
 
-// const from = "no-reply <vagabond.co.usa>";
-
-// const readHTMLFile = async (path) => {
-//   try {
-//     const html = await fs.readFile(path, { encoding: "utf-8" });
-//     return html;
-//   } catch (err) {
-//     throw err;
-//   }
+// var readHTMLFile = function (path, callback) {
+// 	fs.readFile(path, { encoding: "utf-8" }, function (err, html) {
+// 	  if (err) {
+// 	    callback(err);
+// 	  } else {
+// 	    callback(null, html);
+// 	  }
+// 	});
 // };
 
 // module.exports = {
-//   sendEmail: async (templateName, replacements, to, subject, isHTML = false) => {
-//     try {
-//       let content;
-//       if (isHTML) {
-//         const html = await readHTMLFile(templateName);
-//         const template = handlebars.compile(html);
-//         content = template(replacements);
-//       } else {
-//         content = templateName; // Assuming plain text message
-//       }
+// 	sendEmail: (tempateName, replacements, to, subject) => {
+// 	//   const { sender, recipients, subject, html, text } = payload;
+	
+// 	  return new Promise((resolve, reject) => {
+// 			readHTMLFile( tempateName, async function (err, html) {
+// 		if (err) {
+// 			console.log(err);
+// 			return;
+// 		}
+   
+// 	  	var template = await handlebars.compile(html);
+// 	  	htmlToSend = template(replacements);
+// 		  var mailOptions = {
+// 			from: from,
+// 			to: to,
+// 			subject,
+// 			html: htmlToSend
+// 		  };
+// 	  	transporter.sendMail(mailOptions, (error, info) => {
+// 			if (error) {
+// 			  return reject(error);
+// 			}
+// 			return resolve(info);
+// 		  });
+// 	});
+// 	  });
+// 	},
+//   };
+  
 
-//       const mailOptions = {
-//         from: from,
-//         to: to,
-//         subject,
-//         [isHTML ? 'html' : 'text']: content,
-//       };
+const nodeMailer = require("nodemailer");
+const handlebars = require("handlebars");
 
-//       const info = await transporter.sendMail(mailOptions);
-//       return info;
-//     } catch (error) {
-//       throw error;
-//     }
-//   },
-// };
-   
-   {/*   this code is commited due to vercel issue of not sending the html templates it's not code issue  */}
+const transporter = nodeMailer.createTransport({
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true,
+  auth: {
+	user: `${process.env.MAILER_USER}`,
+	pass: `${process.env.MAILER_PASSWORD}`,
+  },
+});
 
-   const nodeMailer = require("nodemailer");
-   const handlebars = require("handlebars");
-   
-   const transporter = nodeMailer.createTransport({
-	 host: "smtp.gmail.com",
-	 port: 465,
-	 secure: true,
-	 auth: {
-	   user: `${process.env.MAILER_USER}`,
-	   pass: `${process.env.MAILER_PASSWORD}`,
-	 },
-   });
-   
-   const from = "no-reply <vagabond.co.usa>";
-   
-   module.exports = {
-	 sendEmail: (htmlContent, replacements, to, subject) => {
-	   return new Promise((resolve, reject) => {
-		 const template = handlebars.compile(htmlContent);
-		 const htmlToSend = template(replacements);
-		 const mailOptions = {
-		   from: from,
-		   to: to,
-		   subject,
-		   html: htmlToSend
-		 };
-   
-		 transporter.sendMail(mailOptions, (error, info) => {
-		   if (error) {
-			 return reject(error);
-		   }
-		   return resolve(info);
-		 });
-	   });
-	 }
-   };
-   
+const from = "no-reply <vagabond.co.usa>";
+
+module.exports = {
+  sendEmail: (htmlContent, replacements, to, subject) => {
+	return new Promise((resolve, reject) => {
+	  const template = handlebars.compile(htmlContent);
+	  const htmlToSend = template(replacements);
+	  const mailOptions = {
+		from: from,
+		to: to,
+		subject,
+		html: htmlToSend
+	  };
+
+	  transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+		  return reject(error);
+		}
+		return resolve(info);
+	  });
+	});
+  }
+};
+
+
